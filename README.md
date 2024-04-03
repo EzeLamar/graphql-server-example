f07ea764f5cd89583e7bbaa090796a64b734529a
ezelamar <ezequiel.lamarquee@gmail.com>
2024-04-03 19:25:25 -0300

    Initial commit: Lesson 1-7

---
+++ README.md
@@ -0,0 +1,8 @@
+# GraphQL Server Example
+Tutorial followed by GraphQL Documentation and NetNinja course to learn how to use the GraphQL and Apollo Server Libraries.
+
+## Apollo Server
+Run the next command to execute the Apollo Server on port 4000:
+```
+node index
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// db
+import db from './_db.js';
+
+// types
+import { typeDefs } from './schema.js';
+
+// resolvers
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        reviews() {
+            return db.reviews;
+        },
+        authors() {
+            return db.authors;
+        },
+        review(_, args) {
+            return db.reviews.find(review => review.id === args.id);
+        },
+        game(_, args) {
+            return db.games.find(game => game.id === args.id);
+        },
+        author(_, args) {
+            return db.authors.find(author => author.id === args.id);
+        }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter(review => review.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter(review => review.author_id === parent.id);
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find(author => author.id === parent.author_id);
+        },
+        game(parent) {
+            return db.games.find(game => game.id === parent.game_id);
+        }
+    }
+};
+
+//server setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log('Server ready at port', 4000);
